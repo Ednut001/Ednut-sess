b5d812ee3b923c99db7d8649642331b3784d892a
Ednut <147618496+Ednut001@users.noreply.github.com>
2024-06-22 02:37:10 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -54,7 +54,7 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'EDNUT-MD;;;' + b64data });
+               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'ednut-bot;;;' + b64data });
 
                let SIGMA_MD_TEXT = `
 ┌⟝——————◀︎ 𝙴𝙳𝙽𝚄𝚃-𝙼𝙳 🔥
